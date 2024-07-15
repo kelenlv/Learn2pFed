a42c0384345b372e24c18f59576b8f585a3bb711
lvkexin <Kelen_Lv@sjtu.edu.cn>
2024-07-15 16:22:44 +0800

    Update demo.sh

--- demo.sh
+++ demo.sh
@@ -5,6 +5,6 @@ n_coefficients=1
 
 mkdir -p ./log/${dataset}_plot
 
-nohup python -u pFL_poly_new.py  --alg $alg --n_coefficients $n_coefficients  --dataset $dataset --n_clients $client   > ./log/${dataset}_plot/${alg}_${n_coefficients}.log 2>&1 &
+nohup python -u demo.py  --alg $alg --n_coefficients $n_coefficients  --dataset $dataset --n_clients $client   > ./log/${dataset}_plot/${alg}_${n_coefficients}.log 2>&1 &
 
 echo "[^_^]: finish"
